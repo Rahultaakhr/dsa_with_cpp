89be18e50aa80fae3aeb93391a3367eb3fa5afb5
rahultaakhr <rahultaakhr@gmail.com>
2024-06-01 17:56:56 +0530

    done

--- lesson_1/data_types.cpp
+++ lesson_1/data_types.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 using namespace std;
-int main (){
-    cout << "Hello Rahul ";
+int main()
+{
+    
+    cout << "Hello World" << endl;
+    cout << "Namsate Duniya" << endl;
+    cout << "Hello Rahul " << endl;
+
+    // integer data type
+    int a = 20;
+    cout << a << endl;
+
+    // float data type
+    float b = 20.3;
+    cout << b << endl;
+
+    // char data type
+    char ch = 'a';
+    cout << ch << endl;
+
+// long data type
+    long A =56;
+    cout << A << endl;
+
+
+    // type conversion
+
+    int CH= 'A';
+    cout << CH << endl;
+
+    int CH2= 'a';
+    cout << CH2 << endl;
+
+    float CH3 ='m';
+    cout << CH3 << endl;
+
+
 }

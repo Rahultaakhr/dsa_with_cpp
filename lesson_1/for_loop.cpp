833583463881fb575985b5b47acc6a50d18dae88
rahultaakhr <rahultaakhr@gmail.com>
2024-06-02 17:39:53 +0530

    done

--- lesson_1/for_loop.cpp
+++ lesson_1/for_loop.cpp
@@ -8,16 +8,32 @@ int main()
     //    cout << i << endl;
     // }
 
-
     // **** factorial of n number ****
 
     int n;
     cout << "Enter n: ";
     cin >> n;
-    int fact = 1;
-    for (int i = 1; i <= n; i++)
+    // int fact = 1;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     fact = fact * i;
+    // }
+    // cout << fact;
+
+    int i = 1;
+    int count = 2;
+    bool isPrime = 1;
+    while (i <= n)
     {
-        fact = fact * i;
+        if (i % 2 != 0)
+        {
+            // cout << i << endl;
+            if (i % count != 0)
+            {
+                cout << i << endl;
+                count = count + 1;
+            }
+        }
+        i++;
     }
-    cout << fact;
 }

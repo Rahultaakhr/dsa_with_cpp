2f97eb83231790f4f853154b46d846c9a00112a8
rahultaakhr <rahultaakhr@gmail.com>
2024-06-02 17:49:20 +0530

    pattern 4

--- lesson_2/pattern3.cpp
+++ lesson_2/pattern3.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout << "Enter n: ";
-    cin >> n;
+   int n;
+   cout << "Enter n: ";
+   cin >> n;
 
-    int i=1;
-     while (i<=n)
-     {
-        int j=1;
-        
-
-     }
-     
+   int i = 1;
+   while (i <= n)
+   {
+      int j = 1;
+      while (j <= i)
+      {
+         cout << (i - j + 1) << " ";
+         j++;
+      }
+      cout << endl;
+      i++;
+   }
 }

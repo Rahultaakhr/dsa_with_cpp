9e0002771f93982b7d6e238f564eed70438be6f6
rahultaakhr <rahultaakhr@gmail.com>
2024-06-03 19:33:34 +0530

    pattern 13

---
+++ lesson_2/pattern13.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    int i=1;
+    while (i<=n)
+    {
+        int j=1;
+        int space=1;
+        int start=n-i+1;
+
+       while (space<=(i-1))
+       {
+        cout<<" ";
+        space++;
+       }
+       
+       while (j<=start)
+       {
+        cout <<"*";
+        j++;
+       }
+       cout << endl;
+       i++;
+       
+        
+    }
+    
+
+}
